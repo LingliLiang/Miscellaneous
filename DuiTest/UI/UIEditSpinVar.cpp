72e7d55d35dd0a0da5393687a17bcd92929bf41f
LingliLiang <linglilias@hotmail.com>
2017-10-20 18:05:17 +0800

    add editspin control

---
+++ DuiTest/UI/UIEditSpinVar.cpp
@@ -0,0 +1,576 @@
+#include "stdafx.h"
+#include "UIEdit.h"
+#include "UIEditSpinVar.h"
+
+//#define  DUI_CTR_EDITSPIN                       (_T("EditSpinVar")) uidefine.h
+//else if (_tcscmp(pstrClass, DUI_CTR_EDITSPIN) == 0)	  pControl = new CEditSpinVarUI; uidlgbuilder.cpp
+//#include "Control/UIEditSpinVar.h" uilib.h
+
+namespace DirectUI
+{
+	CEditSpinVarUI::CEditSpinVarUI()
+		:m_dwDropColor(0xff3F3F3F),
+		m_bEnableSpin(TRUE),m_uSpinAlign(AL_RIGHT),m_uSpinSlide(),
+		m_uSpin1State(0),m_uSpin2State(0),m_varPrecise(1),m_varType(0),
+		m_varStep(0.0),m_bAutoSpin(FALSE)
+	{
+		SetBkColor(0xff000000);
+		SetNativeBkColor(0xff262626);
+		SetNativeEditBkColor(0xff262626);
+		OnNotify += MakeDelegate(this, &CEditSpinVarUI::OnInnerNotify);
+		::memset(&m_unVar,0,sizeof(m_unVar));
+	}
+
+	LPCTSTR CEditSpinVarUI::GetClass() const
+	{
+		return _T("EditSpinVarUI");
+	}
+
+	LPVOID CEditSpinVarUI::GetInterface(LPCTSTR pstrName)
+	{
+		if (_tcscmp(pstrName, DUI_CTR_EDITSPIN) == 0)
+			return static_cast<CEditSpinVarUI*>(this);
+
+		return CEditUI::GetInterface(pstrName);
+	}
+
+	void CEditSpinVarUI::DoEvent(TEventUI& eventui)
+	{
+		if (!IsMouseEnabled() && eventui.Type > UIEVENT__MOUSEBEGIN && eventui.Type < UIEVENT__MOUSEEND) 
+		{
+			if (m_pParent != NULL) m_pParent->DoEvent(eventui);
+			else CLabelUI::DoEvent(eventui);
+			return;
+		}
+		if (eventui.Type == UIEVENT_SETCURSOR && IsEnabled())
+		{
+			if(::PtInRect(&m_rcSpin[0],eventui.ptMouse) || ::PtInRect(&m_rcSpin[1],eventui.ptMouse))
+			{
+				::SetCursor(::LoadCursor(NULL, MAKEINTRESOURCE(IDC_HAND)));
+			}
+			else 
+				::SetCursor(::LoadCursor(NULL, MAKEINTRESOURCE(IDC_IBEAM)));
+			return;
+		}
+		if (eventui.Type == UIEVENT_SCROLLWHEEL)
+		{
+			if (m_pWindow != NULL) {
+				m_pWindow->Close();
+			}return;
+		}
+		if( eventui.Type == UIEVENT_BUTTONDOWN || eventui.Type == UIEVENT_DBLCLICK )
+		{
+			if(IsEnabled())
+			{
+				BOOL bInSpin = FALSE;
+				if( ::PtInRect(&m_rcSpin[0],eventui.ptMouse)) {
+					m_uSpin1State |= UISTATE_PUSHED | UISTATE_CAPTURED;
+					Invalidate();
+					bInSpin = TRUE;
+				}
+				else if(::PtInRect(&m_rcSpin[1],eventui.ptMouse))
+				{
+					m_uSpin2State |= UISTATE_PUSHED | UISTATE_CAPTURED;
+					Invalidate();
+					bInSpin = TRUE;					
+				}
+				if(bInSpin){
+					if (m_pWindow != NULL) {
+						m_pWindow->Close();
+					}
+					return;
+				} //bInSpin
+			}
+		}
+		if( eventui.Type == UIEVENT_MOUSEMOVE )
+		{
+			BOOL bInSpin1 = ::PtInRect(&m_rcSpin[0], eventui.ptMouse);
+			BOOL bInSpin2 = bInSpin1 ? FALSE : ::PtInRect(&m_rcSpin[1], eventui.ptMouse);
+
+			if(bInSpin1 && !(m_uSpin1State & UISTATE_HOT)){
+				m_uSpin1State |= UISTATE_HOT;
+				Invalidate();
+			}
+			else if(!bInSpin1 && (m_uSpin1State & UISTATE_HOT))
+			{
+				m_uSpin1State &= ~UISTATE_HOT;
+				Invalidate();
+			}
+			if(bInSpin2 && !(m_uSpin2State & UISTATE_HOT)){
+				m_uSpin2State |= UISTATE_HOT;
+				Invalidate();
+			}
+			else if(!bInSpin2 && (m_uSpin2State & UISTATE_HOT))
+			{
+				m_uSpin2State &= ~UISTATE_HOT;
+				Invalidate();
+			}
+
+			if( m_uSpin1State & UISTATE_CAPTURED ) {
+				if(bInSpin1) m_uSpin1State |= UISTATE_PUSHED;
+				else m_uSpin1State &= ~UISTATE_PUSHED;
+				Invalidate();
+			}
+			else if( m_uSpin2State & UISTATE_CAPTURED ) {
+				if( bInSpin2) m_uSpin2State |= UISTATE_PUSHED;
+				else m_uSpin2State &= ~UISTATE_PUSHED;
+				Invalidate();
+			}
+			return;
+		}
+		if( eventui.Type == UIEVENT_BUTTONUP )
+		{
+			if( (m_uSpin1State & UISTATE_CAPTURED) != 0 ) {
+				if( ::PtInRect(&m_rcSpin[0], eventui.ptMouse) ) ActiveSpin(0);
+				m_uSpin1State &= ~(UISTATE_PUSHED | UISTATE_CAPTURED);
+				Invalidate();
+			}
+			else if( (m_uSpin2State & UISTATE_CAPTURED) != 0 ) {
+				if( ::PtInRect(&m_rcSpin[1], eventui.ptMouse) ) ActiveSpin(1);
+				m_uSpin2State &= ~(UISTATE_PUSHED | UISTATE_CAPTURED);
+				Invalidate();
+			}
+			return;
+		}
+		if( eventui.Type == UIEVENT_MOUSELEAVE )
+		{
+			if( IsEnabled() ) {
+				m_uSpin1State &= ~UISTATE_HOT;
+				m_uSpin2State &= ~UISTATE_HOT;
+			}
+		}
+		CEditUI::DoEvent(eventui);
+	}
+
+	bool CEditSpinVarUI::OnInnerNotify(LPVOID lpMsg)
+	{
+		TNotifyUI &Msg = *(static_cast<TNotifyUI*>(lpMsg));
+		if(Msg.sType == DUI_MSGTYPE_TEXTCHANGED)
+		{
+			if(m_varType == 1)
+			{
+				int vInt = 0;
+				auto var = _stscanf_s(m_sText.GetData(),_T("%d"), &vInt);
+				if(var){
+					m_unVar.varInt = vInt;
+				}
+				else
+				{
+						m_sText.Format(_T("%d"),m_unVar.varInt);
+				}
+			}
+			else if(m_varType == 2)
+			{
+				double vDouble = 0;
+				auto var = _stscanf_s(m_sText.GetData(),_T("%lf"), &vDouble);
+				if(var){
+					m_unVar.varDouble = vDouble;
+				}
+				else
+				{
+					TCHAR fmt[64] = {0};
+					auto var2 = _stprintf_s(fmt,_T("%%.%dlf"),m_varPrecise);
+					if(var2){
+						m_sText.Format(fmt,m_unVar.varDouble);
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	LPCTSTR CEditSpinVarUI::GetEmptyText()
+	{
+		return m_sEmptyText;
+	}
+
+	void CEditSpinVarUI::SetEmptyText(LPCTSTR pStrText)
+	{
+		m_sEmptyText = pStrText;
+		Invalidate();
+	}
+
+	void CEditSpinVarUI::SetDropColor(DWORD dwTextColor)
+	{
+		m_dwDropColor = dwTextColor;
+		Invalidate();
+	}
+
+	void CEditSpinVarUI::SetAttribute(LPCTSTR pstrName, LPCTSTR pstrValue)
+	{
+		if( _tcscmp(pstrName, _T("emptytext")) == 0 ) SetEmptyText(pstrValue);
+		else if( _tcscmp(pstrName, _T("headtext")) == 0 ) SetHeadText(pstrValue);
+		else if (_tcscmp(pstrName, _T("etdropcolor")) == 0)
+		{
+			if (*pstrValue == _T('#'))
+				pstrValue = ::CharNext(pstrValue);
+
+			LPTSTR pstr = NULL;
+			DWORD clrColor = _tcstoul(pstrValue, &pstr, 16);
+			SetDropColor(clrColor);
+		}
+		else if( _tcscmp(pstrName, _T("enablespin")) == 0 )
+		{
+			(_tcscmp(pstrValue, _T("true")) != 0) ? m_bEnableSpin = FALSE : m_bEnableSpin = TRUE;
+		}
+		else if( _tcscmp(pstrName, _T("spinslide")) == 0 ) 
+		{
+			LPTSTR pstr = NULL;
+			m_uSpinSlide.cx = _tcstoul(pstrValue, &pstr, 10);
+			m_uSpinSlide.cy = _tcstoul(pstr+1, &pstr, 10);
+		}
+		else if( _tcscmp(pstrName, _T("spinalign")) == 0 ) 
+		{
+			LPTSTR pstr = NULL;
+			m_uSpinAlign = _tcstoul(pstrValue, &pstr, 10);
+		}
+		else if( _tcscmp(pstrName, _T("spin1normalimage")) == 0 ) SetSpin1NormalImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("spin1hotimage")) == 0 ) SetSpin1HotImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("spin1pushedimage")) == 0 ) SetSpin1PushedImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("spin1disabledimage")) == 0 ) SetSpin1DisabledImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("spin2normalimage")) == 0 ) SetSpin2NormalImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("spin2hotimage")) == 0 ) SetSpin2HotImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("spin2pushedimage")) == 0 ) SetSpin2PushedImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("spin2disabledimage")) == 0 ) SetSpin2DisabledImage(pstrValue);
+		else if( _tcscmp(pstrName, _T("precise")) == 0 )
+		{
+			SetValueType(m_varType, _ttoi(pstrValue));
+		}
+		else if( _tcscmp(pstrName, _T("vtype")) == 0 )
+		{
+			SetValueType(_ttoi(pstrValue), m_varPrecise);
+		}
+		else if( _tcscmp(pstrName, _T("value")) == 0 )
+		{
+			if(m_varType == 1) SetValue(_ttoi(pstrValue));
+			else if(m_varType == 2) SetValue(_tstof(pstrValue));			
+		}
+		else if( _tcscmp(pstrName, _T("vstep")) == 0 )
+		{
+			SetValueStep(_tstof(pstrValue));			
+		}
+		else if( _tcscmp(pstrName, _T("autospin")) == 0 )
+		{
+			(_tcscmp(pstrValue, _T("true")) != 0) ? m_bAutoSpin = FALSE : m_bAutoSpin = TRUE;		
+		}
+		else CEditUI::SetAttribute(pstrName, pstrValue);
+	}
+
+	void CEditSpinVarUI::SetPos(RECT rc)
+	{
+		//not need call surper SetPos()
+		CControlUI::SetPos(rc);
+		if( m_pWindow != NULL ) {
+			RECT rcPos = m_pWindow->CalPos();
+			::SetWindowPos(m_pWindow->GetHWND(), NULL, rcPos.left, rcPos.top, rcPos.right - rcPos.left, 
+				rcPos.bottom - rcPos.top, SWP_NOZORDER | SWP_NOACTIVATE);        
+		}
+		CalcSpinPos();
+	}
+
+	void CEditSpinVarUI::CalcSpinPos()
+	{
+		CRect rcItem = m_rcItem;
+		rcItem.Inflate(&m_rcPadding);
+		auto subHeight = (rcItem.GetHeight() - m_uSpinSlide.cy*2)/2;
+		m_rcSpin[0] = CRect(0,0,m_uSpinSlide.cx,m_uSpinSlide.cy);
+		m_rcSpin[1] = CRect(0,0,m_uSpinSlide.cx,m_uSpinSlide.cy);
+		if(m_uSpinAlign == AL_LEFT)
+		{
+			m_rcSpin[0].MoveToXY(m_rcItem.left,m_rcItem.top + subHeight);
+			m_rcSpin[1].MoveToXY(m_rcItem.left,m_rcItem.top + subHeight+m_uSpinSlide.cy);
+			if(m_rcTextPadding.left < m_uSpinSlide.cx)
+			{
+				m_rcTextPadding.left += m_uSpinSlide.cx;
+			}
+		}
+		else if(m_uSpinAlign == AL_RIGHT)
+		{
+			m_rcSpin[0].MoveToXY(m_rcItem.right - m_uSpinSlide.cx,m_rcItem.top + subHeight);
+			m_rcSpin[1].MoveToXY(m_rcItem.right - m_uSpinSlide.cx,m_rcItem.top + subHeight+m_uSpinSlide.cy);
+			if(m_rcTextPadding.right < m_uSpinSlide.cx)
+			{
+				m_rcTextPadding.right += m_uSpinSlide.cx;
+			}
+		}
+		else if(m_uSpinAlign == AL_SPLIT)
+		{
+			m_rcSpin[0].MoveToXY(m_rcItem.left,m_rcItem.top + subHeight);
+			m_rcSpin[1].MoveToXY(m_rcItem.right - m_uSpinSlide.cx,m_rcItem.top + subHeight+m_uSpinSlide.cy);
+			if(m_rcTextPadding.left < m_uSpinSlide.cx)
+			{
+				m_rcTextPadding.left += m_uSpinSlide.cx;
+			}
+			if(m_rcTextPadding.right < m_uSpinSlide.cx)
+			{
+				m_rcTextPadding.right += m_uSpinSlide.cx;
+			}
+		}
+	}
+
+	void CEditSpinVarUI::PaintStatusImage(HDC hDC)
+	{
+		CEditUI::PaintStatusImage(hDC);
+		if(!m_bEnableSpin) return;
+		if( !IsEnabled() ){
+			m_uSpin1State |= UISTATE_DISABLED;
+			m_uSpin2State |= UISTATE_DISABLED;
+		}
+		else{
+			m_uSpin1State &= ~UISTATE_DISABLED;
+			m_uSpin2State &= ~UISTATE_DISABLED;
+		}
+		PaintSpin(hDC,m_uSpin1State,m_sSpin1Image,m_rcSpin[0]);
+		PaintSpin(hDC,m_uSpin2State,m_sSpin2Image,m_rcSpin[1]);
+	}
+	
+	void CEditSpinVarUI::PaintImg(HDC& hDC, CUIString &strImg, RECT* rectPaint /*= NULL*/)
+	{
+		bool sucss = false;
+		if(strImg.IsEmpty()) return;
+		if(rectPaint)
+		{
+			sucss = CRenderEngine::DrawImageString(hDC, m_pManager,*rectPaint, *rectPaint, strImg.GetData(), NULL);
+		}
+		else
+		{
+			sucss = CRenderEngine::DrawImageString(hDC, m_pManager, m_rcItem, m_rcPaint, strImg.GetData(), NULL);
+		}
+		if(!sucss) strImg.Empty();
+	}
+
+	void CEditSpinVarUI::PaintSpin(HDC& hDC, UINT state, CUIString (&spinImg)[4], RECT rcPaint)
+	{
+		if(state & UISTATE_DISABLED)
+		{
+			PaintImg(hDC, spinImg[ID_DIS], &rcPaint);
+		}
+		else if(state & UISTATE_PUSHED)
+		{
+			PaintImg(hDC, spinImg[ID_PUSH], &rcPaint);
+		}
+		else if(state & UISTATE_HOT)
+		{
+			PaintImg(hDC, spinImg[ID_HOT], &rcPaint);
+		}
+		else
+		{
+			PaintImg(hDC, spinImg[ID_NOR], &rcPaint);
+		}
+	}
+
+	void CEditSpinVarUI::PaintText(HDC hDC)
+	{
+		if( m_dwTextColor == 0 ) m_dwTextColor = m_pManager->GetDefaultFontColor();
+		if( m_dwDisabledTextColor == 0 ) m_dwDisabledTextColor = m_pManager->GetDefaultDisabledColor();
+
+		CUIString sText = m_sText;
+		bool emptyText = m_sText.IsEmpty();
+
+		if (emptyText && !m_sEmptyText.IsEmpty())
+		{
+			sText = m_sEmptyText;
+		}
+
+		if( m_bPasswordMode ) {
+			if (!emptyText){
+				sText.Empty();
+				LPCTSTR p = m_sText.GetData();
+				while( *p != _T('\0') ) {
+					sText += m_cPasswordChar;
+					p = ::CharNext(p);
+				}
+			}
+		}
+
+		RECT rc = m_rcItem;
+		rc.left += m_rcTextPadding.left;
+		rc.right -= m_rcTextPadding.right;
+		rc.top += m_rcTextPadding.top;
+		rc.bottom -= m_rcTextPadding.bottom;
+
+		if(!m_sHeadText.IsEmpty())
+		{
+			RECT rcHead = rc;
+			SIZE szHead = CRenderEngine::GetTextSize(hDC,m_pManager,m_sHeadText.GetData(),m_iFont,DT_VCENTER | DT_LEFT);
+			rc.left += szHead.cx;
+			rcHead.right += szHead.cx;
+			if( IsEnabled() ) {
+				CRenderEngine::DrawText(hDC, m_pManager, rcHead, m_sHeadText, emptyText ? m_dwTextColor-m_dwDropColor : m_dwTextColor, \
+					m_iFont, DT_SINGLELINE | DT_VCENTER | DT_LEFT);
+			}
+			else {
+				CRenderEngine::DrawText(hDC, m_pManager, rcHead, m_sHeadText, emptyText ? m_dwTextColor - m_dwDropColor : m_dwDisabledTextColor, \
+					m_iFont, DT_SINGLELINE | DT_VCENTER | DT_LEFT);
+			}
+		}
+
+		if(m_varType == 1)
+		{
+			sText.Format(_T("%d"),m_unVar.varInt);
+			m_sText = sText;
+		}
+		else if(m_varType == 2)
+		{
+			TCHAR fmt[64] = {0};
+			auto var = _stprintf_s(fmt,_T("%%.%dlf"),m_varPrecise);
+			if(var){
+				sText.Format(fmt,m_unVar.varDouble);
+				m_sText = sText;
+			}
+		}
+
+		if( IsEnabled() ) {
+			CRenderEngine::DrawText(hDC, m_pManager, rc, sText, emptyText ? m_dwTextColor-m_dwDropColor : m_dwTextColor, \
+				m_iFont, DT_SINGLELINE | m_uTextStyle);
+		}
+		else {
+			CRenderEngine::DrawText(hDC, m_pManager, rc, sText, emptyText ? m_dwTextColor - m_dwDropColor : m_dwDisabledTextColor, \
+				m_iFont, DT_SINGLELINE | m_uTextStyle);
+		}
+	}
+
+	void CEditSpinVarUI::ActiveSpin(int index)
+	{
+		if(m_bAutoSpin)
+		{	
+			if(m_varType == 1)
+			{
+				index ? m_unVar.varInt -= (int)m_varStep : m_unVar.varInt += (int)m_varStep;
+			}
+			else if(m_varType == 2)
+			{
+				index ? m_unVar.varDouble -= m_varStep : m_unVar.varDouble += m_varStep;
+			}
+		}
+		if( m_pManager != NULL ) m_pManager->SendNotify(this, DUI_MSGTYPE_CLICK, index, m_bAutoSpin);
+	}
+
+	LPCTSTR CEditSpinVarUI::GetSpin1NormalImage()
+	{
+		return m_sSpin1Image[ID_NOR];
+	}
+	void CEditSpinVarUI::SetSpin1NormalImage(LPCTSTR pStrImage)
+	{
+		m_sSpin1Image[ID_NOR] = pStrImage;
+	}
+	LPCTSTR CEditSpinVarUI::GetSpin1HotImage()
+	{
+		return m_sSpin1Image[ID_HOT];
+	}
+	void CEditSpinVarUI::SetSpin1HotImage(LPCTSTR pStrImage)
+	{
+		m_sSpin1Image[ID_HOT] = pStrImage;
+	}
+	LPCTSTR CEditSpinVarUI::GetSpin1PushedImage()
+	{
+		return m_sSpin1Image[ID_PUSH];
+	}
+	void CEditSpinVarUI::SetSpin1PushedImage(LPCTSTR pStrImage)
+	{
+		m_sSpin1Image[ID_PUSH] = pStrImage;
+	}
+	LPCTSTR CEditSpinVarUI::GetSpin1DisabledImage()
+	{
+		return m_sSpin1Image[ID_DIS];
+	}
+	void CEditSpinVarUI::SetSpin1DisabledImage(LPCTSTR pStrImage)
+	{
+		m_sSpin1Image[ID_DIS] = pStrImage;
+	}
+
+	LPCTSTR CEditSpinVarUI::GetSpin2NormalImage()
+	{
+		return m_sSpin2Image[ID_NOR];
+	}
+	void CEditSpinVarUI::SetSpin2NormalImage(LPCTSTR pStrImage)
+	{
+		m_sSpin2Image[ID_NOR] = pStrImage;
+	}
+	LPCTSTR CEditSpinVarUI::GetSpin2HotImage()
+	{
+		return m_sSpin2Image[ID_HOT];
+	}
+	void CEditSpinVarUI::SetSpin2HotImage(LPCTSTR pStrImage)
+	{
+		m_sSpin2Image[ID_HOT] = pStrImage;
+	}
+	LPCTSTR CEditSpinVarUI::GetSpin2PushedImage()
+	{
+		return m_sSpin2Image[ID_PUSH];
+	}
+	void CEditSpinVarUI::SetSpin2PushedImage(LPCTSTR pStrImage)
+	{
+		m_sSpin2Image[ID_PUSH] = pStrImage;
+	}
+	LPCTSTR CEditSpinVarUI::GetSpin2DisabledImage()
+	{
+		return m_sSpin2Image[ID_DIS];
+	}
+	void CEditSpinVarUI::SetSpin2DisabledImage(LPCTSTR pStrImage)
+	{
+		m_sSpin2Image[ID_DIS] = pStrImage;
+	}
+
+	void CEditSpinVarUI::SetHeadText(LPCTSTR pStrFormat)
+	{
+		m_sHeadText = pStrFormat;
+	}
+	LPCTSTR CEditSpinVarUI::GetHeadText()
+	{
+		return m_sHeadText;
+	}
+
+	void CEditSpinVarUI::SetValueType(unsigned int type, unsigned short precise /*= 1*/)
+	{
+		if(type < 3){
+			m_varType = type;
+			m_varPrecise = precise;
+		}
+	}
+	unsigned int CEditSpinVarUI::GetValueType() const
+	{
+		return m_varType;
+	}
+	bool CEditSpinVarUI::SetValue(int value)
+	{
+		if(m_varType == 1)
+		{
+			m_unVar.varInt = value;
+			Invalidate();
+			return true;
+		}
+		return false;
+	}
+	bool CEditSpinVarUI::SetValue(double value)
+	{
+		if(m_varType == 2)
+		{
+			m_unVar.varDouble = value;
+			Invalidate();
+			return true;
+		}
+		return false;
+	}
+	int CEditSpinVarUI::GetValue() const
+	{
+		if(m_varType == 1)
+		{
+			return m_unVar.varInt;
+		}
+		return 0;
+	}
+	double CEditSpinVarUI::GetValue(double) const
+	{
+		if(m_varType == 2)
+		{
+			return m_unVar.varDouble;
+		}
+		return 0.0;
+	}
+	void CEditSpinVarUI::SetValueStep(double value)
+	{
+		m_varStep = value;
+	}
+
+} //namespace DirectUI
---
+++ DuiTest/UI/UIEditSpinVar.h
@@ -0,0 +1,118 @@
+#ifndef __UIEDITEMPTYVAR_H__
+#define __UIEDITEMPTYVAR_H__
+
+#pragma once
+
+namespace DirectUI
+{
+	class CEditUI;
+	class CEditSpinVarUI;
+	class CEditWnd;
+	/**
+	<EditSpinVar parent="Edit" notifies="click setfocus killfocus timer menu return textchanged windowinit(root)">
+	<!--注意: 该控件按下SPIN会有消息DUI_MSGTYPE_CLICK, wparam 为 spin按钮id,0为增按钮,1为减按钮, lparam为是否设置了自动增减-->
+	<Attribute name="emptytext" default="" type="STRING" comment="为空时显示的字串"/>
+	<Attribute name="etdropcolor" default="0x00000000" type="DWORD" comment="为空时当前字体颜色退色的程度,如(0x00111111)"/>
+	<Attribute name="headtext" default="" type="STRING" comment="显示再编辑框左边的说明字串, 如 [ 这是headtext:      这是显示字串 ]"/>
+	<Attribute name="enablespin" default="true" type="BOOL" comment="是否启用SPIN按钮,如(false)"/>
+	<Attribute name="autospin" default="false" type="BOOL" comment="是否启用自增减数值,如(false), 当按下SPIN按钮时进行数值(数据类型及步伐幅度需要设置)的自增减"/>
+	<Attribute name="vtype" default="0" type="INT" comment="数据类型, 0-原始 1-INT 2-double"/>
+	<Attribute name="value" default="0" type="INT,DOUBLE" comment="数据类型值, 如(1或1.00)"/>
+	<Attribute name="vstep" default="0.0" type="DOUBLE" comment="数据步伐幅度值, 如(1.00)"/>
+	<Attribute name="precise" default="1" type="INT" comment="double数据精度, 小数点位置"/>
+	<Attribute name="spinslide" default="0,0" type="SIZE" comment="SPIN按钮图片宽高, 如(20,20)"/>
+	<Attribute name="spinalign" default="2" type="INT" comment="SPIN按钮对齐方式, 0-放置在左边 1-左右分别放置一个 2-放置在右边"/>
+	<Attribute name="spin1normalimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	<Attribute name="spin1hotimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	<Attribute name="spin1pushedimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	<Attribute name="spin1disabledimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	<Attribute name="spin2normalimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	<Attribute name="spin2hotimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	<Attribute name="spin2pushedimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	<Attribute name="spin2disabledimage" default="" type="STRING" comment="SPIN按钮状态图片"/>
+	</EditSpinVar>
+	**/
+	class DirectUI_API CEditSpinVarUI : public CEditUI
+	{
+		friend class CEditWnd;
+	public:
+		CEditSpinVarUI();
+
+		LPCTSTR GetClass() const;
+		LPVOID GetInterface(LPCTSTR pstrName);
+
+		LPCTSTR GetEmptyText();
+		void SetEmptyText(LPCTSTR pStrText);
+		void SetDropColor(DWORD dwTextColor);
+
+		void DoEvent(TEventUI& eventui);
+		void SetAttribute(LPCTSTR pstrName, LPCTSTR pstrValue);
+
+		virtual void PaintStatusImage(HDC hDC);
+		virtual void PaintText(HDC hDC);
+		virtual void SetPos(RECT rc);
+
+		LPCTSTR GetSpin1NormalImage();
+		void SetSpin1NormalImage(LPCTSTR pStrImage);
+		LPCTSTR GetSpin1HotImage();
+		void SetSpin1HotImage(LPCTSTR pStrImage);
+		LPCTSTR GetSpin1PushedImage();
+		void SetSpin1PushedImage(LPCTSTR pStrImage);
+		LPCTSTR GetSpin1DisabledImage();
+		void SetSpin1DisabledImage(LPCTSTR pStrImage);
+
+		LPCTSTR GetSpin2NormalImage();
+		void SetSpin2NormalImage(LPCTSTR pStrImage);
+		LPCTSTR GetSpin2HotImage();
+		void SetSpin2HotImage(LPCTSTR pStrImage);
+		LPCTSTR GetSpin2PushedImage();
+		void SetSpin2PushedImage(LPCTSTR pStrImage);
+		LPCTSTR GetSpin2DisabledImage();
+		void SetSpin2DisabledImage(LPCTSTR pStrImage);
+
+		void SetHeadText(LPCTSTR pStr);
+		LPCTSTR GetHeadText();
+
+		/*0 - raw ,1 - int ,2 - double*/
+		void SetValueType(unsigned int type, unsigned short precise = 1);
+		unsigned int GetValueType() const;
+		bool SetValue(int value);
+		bool SetValue(double value);
+		int GetValue() const;
+		double GetValue(double) const;
+		void SetValueStep(double value);
+	protected:
+		void CalcSpinPos();
+		void PaintSpin(HDC& hDC, UINT state, CUIString (&spinImg)[4], RECT rcPaint);
+		void PaintImg(HDC& hDC, CUIString &strImg, RECT* rectPaint = NULL);
+		void ActiveSpin(int index);
+
+		bool OnInnerNotify(LPVOID lpMsg);
+
+		CUIString m_sHeadText; // 指定显示在左部的说明文字
+		CUIString m_sEmptyText; //为空时显示的文字
+		DWORD	m_dwDropColor; //EmptyText的颜色
+
+		enum BTN_INDEX{ID_NOR, ID_HOT, ID_PUSH,ID_DIS};
+		enum SPIN_ALIGN{AL_LEFT = 1, AL_RIGHT, AL_SPLIT};
+		union {
+			int varInt;
+			double varDouble;
+		} m_unVar;
+		UINT m_varType; // 0 - raw 1 - int 2 - double
+		unsigned short m_varPrecise; // varDouble
+		double m_varStep;
+		//增减计数按钮
+		BOOL m_bEnableSpin; //启用Spin按钮
+		BOOL m_bAutoSpin; //启用数值自增减
+		UINT m_uSpinAlign; // left/right/split 对齐方式
+		SIZE m_uSpinSlide; // 大小
+		UINT m_uSpin1State;
+		UINT m_uSpin2State;
+		CRect m_rcSpin[2];
+		CUIString m_sSpin1Image[4];
+		CUIString m_sSpin2Image[4];
+
+	};
+} //namespace DirectUI
+#endif // __UIEDITEMPTYVAR_H__
